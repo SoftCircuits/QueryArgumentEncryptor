94dca064e2255b88a6ae89168e5f90cfcf193d34
Jonathan Wood <jwood@softcircuits.com>
2019-07-03 08:11:39 -0600

    Add project files.

---
+++ QueryArgumentEncryptor/ArgumentEncryptor.cs
@@ -0,0 +1,201 @@
+﻿// Copyright (c) 2019 Jonathan Wood (www.softcircuits.com)
+// Licensed under the MIT license.
+//
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace QueryArgumentEncryptor
+{
+    public class ArgumentEncryptor : Dictionary<string, string>
+    {
+        public string Password { get; set; }
+
+        // Note: The following items must be values that are unlikely to
+        // appear within the user's data
+
+        // Item delimiter
+        protected const char ItemDelimiter = '\u0000';
+        // Key/value delimiter
+        protected const char KeyValueDelimiter = '\u0001';
+        // Key for checksum value
+        protected const string ChecksumKey = "$\u0002$";
+
+        /// <summary>
+        /// Creates an empty dictionary
+        /// </summary>
+        /// <param name="password">Password used to encrypt/decrypt data.</param>
+        public ArgumentEncryptor(string password)
+        {
+            Password = password ?? throw new ArgumentNullException(nameof(password));
+        }
+
+        /// <summary>
+        /// Creates a dictionary and populates it from the given encrypted string.
+        /// </summary>
+        /// <param name="password">Password used to encrypt/decrypt data.</param>
+        /// <param name="encryptedData">Data encrypted with <see cref="EncryptData"></see>.</param>
+        /// <param name="urlDecode">If true, <paramref name="encryptedData"/> is
+        /// URL decoded before being decrypted. In general, this must match
+        /// the setting passed to <see cref="EncryptData"/>.</param>
+        public ArgumentEncryptor(string password, string encryptedData, bool urlDecode = true)
+        {
+            Password = password ?? throw new ArgumentNullException(nameof(password));
+            DecryptData(encryptedData, urlDecode);
+        }
+
+        /// <summary>
+        /// Converts the current key/value pairs to an encrypted string.
+        /// </summary>
+        /// <param name="urlEncode">If true, the resulting encrypted string
+        /// is URL encoded. Set to false to leave string unencoded.</param>
+        /// <returns>The encrypted string.</returns>
+        public string EncryptData(bool urlEncode = true)
+        {
+            // Build query string from current contents
+            StringBuilder content = new StringBuilder();
+            foreach (string key in base.Keys)
+            {
+                if (content.Length > 0)
+                    content.Append(ItemDelimiter);
+                content.AppendFormat("{0}{1}{2}", key, KeyValueDelimiter, this[key]);
+            }
+            // Add checksum
+            if (content.Length > 0)
+                content.Append(ItemDelimiter);
+            content.AppendFormat("{0}{1}{2}", ChecksumKey, KeyValueDelimiter, ComputeChecksum());
+            // Encrypt resulting string
+            string result = Encrypt(content.ToString());
+            return (urlEncode) ? WebUtility.UrlEncode(result) : result;
+        }
+
+        /// <summary>
+        /// Builds the current collection from an encrypted string.
+        /// </summary>
+        /// <param name="encryptedData">The encrypted string.</param>
+        /// <param name="urlDecode">If true, <paramref name="encryptedData"/> is
+        /// URL decoded before being decrypted. In general, this must match
+        /// the setting passed to <see cref="EncryptData"/>.</param>
+        public void DecryptData(string encryptedData, bool urlDecode = true)
+        {
+            // Descrypt string
+            if (urlDecode)
+                encryptedData = WebUtility.UrlDecode(encryptedData);
+            string data = Decrypt(encryptedData);
+            // Parse out key/value pairs and add to dictionary
+            Clear();
+            string checksum = null;
+            string[] keyValues = data.Split(ItemDelimiter);
+            foreach (string keyValue in keyValues)
+            {
+                int i = keyValue.IndexOf(KeyValueDelimiter);
+                if (i != -1)
+                {
+                    string key = keyValue.Substring(0, i);
+                    string value = keyValue.Substring(i + 1);
+                    if (key == ChecksumKey)
+                        checksum = value;
+                    else
+                        Add(key, value);
+                }
+                else Add(keyValue, string.Empty);
+            }
+            // Clear contents if valid checksum not found
+            if (checksum == null || checksum != ComputeChecksum())
+                Clear();
+        }
+
+        /// <summary>
+        /// Returns a simple checksum for all keys and values in the collection
+        /// </summary>
+        /// <returns></returns>
+        protected string ComputeChecksum()
+        {
+            int checksum = 0;
+
+            unchecked
+            {
+                foreach (KeyValuePair<string, string> pair in this)
+                {
+                    checksum += 17;
+                    checksum += pair.Key.GetHashCode();
+                    checksum += 17;
+                    checksum += pair.Value.GetHashCode();
+                }
+            }
+            return checksum.ToString("X");
+        }
+
+        internal string Encrypt(string text)
+        {
+            if (string.IsNullOrWhiteSpace(Password))
+                throw new Exception("No password specified");
+
+            byte[] data = Encoding.UTF8.GetBytes(text);
+
+            using (TripleDES tripleDES = TripleDES.Create())
+            {
+                tripleDES.IV = new byte[8];
+                using (RNGCryptoServiceProvider rngProvider = new RNGCryptoServiceProvider())
+                {
+                    rngProvider.GetBytes(tripleDES.IV);
+                }
+
+                var x = new Rfc2898DeriveBytes(Password, tripleDES.IV);
+                tripleDES.Key = x.GetBytes(16);
+
+                using (MemoryStream memStream = new MemoryStream())
+                {
+                    // Save salt with encrypted data
+                    memStream.Write(tripleDES.IV, 0, tripleDES.IV.Length);
+
+                    using (CryptoStream encryptor = new CryptoStream(memStream, tripleDES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        encryptor.Write(data, 0, data.Length);
+                        encryptor.FlushFinalBlock();
+                        encryptor.Close();
+                    }
+                    return Convert.ToBase64String(memStream.ToArray());
+                }
+            }
+        }
+
+        internal string Decrypt(string cipherString)
+        {
+            if (string.IsNullOrWhiteSpace(Password))
+                throw new Exception("No password specified");
+
+            try
+            {
+                byte[] data = Convert.FromBase64String(cipherString);
+
+                using (TripleDES tripleDES = TripleDES.Create())
+                {
+                    tripleDES.IV = new byte[8];
+                    Array.Copy(data, tripleDES.IV, tripleDES.IV.Length);
+
+                    var x = new Rfc2898DeriveBytes(Password, tripleDES.IV);
+                    tripleDES.Key = x.GetBytes(16);
+
+                    using (MemoryStream memStream = new MemoryStream())
+                    {
+                        using (CryptoStream decryptor = new CryptoStream(memStream, tripleDES.CreateDecryptor(), CryptoStreamMode.Write))
+                        {
+                            decryptor.Write(data, 8, data.Length - 8);
+                            decryptor.Flush();
+                            decryptor.Close();
+                        }
+                        return Encoding.UTF8.GetString(memStream.ToArray());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Invalid password or data", ex);
+            }
+        }
+    }
+}
---
+++ TestQueryArgumentEncryptor/QueryArgumentEncryptorTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QueryArgumentEncryptor;
+using System;
+
+namespace TestQueryArgumentEncryptor
+{
+    [TestClass]
+    public class QueryArgumentEncryptorTests
+    {
+        //[TestMethod]
+        //public void Test()
+        //{
+        //    //ArgumentEncryptor encryptor = new ArgumentEncryptor("Password123");
+        //    ArgumentEncryptor encryptor = PopulateEncryptor("Password123");
+        //    string url = $"http://www.softcircuits.com?d={encryptor.EncryptData()}";
+        //}
+
+        [TestMethod]
+        public void TestData()
+        {
+            ArgumentEncryptor encryptor1 = PopulateEncryptor("Password123");
+            string data = encryptor1.EncryptData();
+            Assert.AreNotEqual(0, data.Length);
+            ArgumentEncryptor encryptor2 = new ArgumentEncryptor("Password123", data);
+            CollectionAssert.AreEqual(encryptor1, encryptor2);
+        }
+
+        [TestMethod]
+        public void TestDataNoUrlEncoding()
+        {
+            ArgumentEncryptor encryptor1 = PopulateEncryptor("Password123");
+            string data = encryptor1.EncryptData(false);
+            Assert.AreNotEqual(0, data.Length);
+            ArgumentEncryptor encryptor2 = new ArgumentEncryptor("Password123", data, false);
+            CollectionAssert.AreEqual(encryptor1, encryptor2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), AllowDerivedTypes = true)]
+        public void TestBadPassword()
+        {
+            ArgumentEncryptor encryptor1 = PopulateEncryptor("Password123");
+            string data = encryptor1.EncryptData();
+            Assert.AreNotEqual(0, data.Length);
+            ArgumentEncryptor encryptor2 = new ArgumentEncryptor("Password456", data);
+            Assert.AreEqual(0, encryptor2.Count);
+            CollectionAssert.AreNotEqual(encryptor1, encryptor2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), AllowDerivedTypes = true)]
+        public void TestBadData()
+        {
+            ArgumentEncryptor encryptor1 = PopulateEncryptor("Password123");
+            string data = encryptor1.EncryptData();
+            Assert.AreNotEqual(0, data.Length);
+            int mid = data.Length / 2;
+            data = data.Substring(0, mid) + "x" + data.Substring(mid);  // Corrupt data
+            ArgumentEncryptor encryptor2 = new ArgumentEncryptor("Password123", data);
+            CollectionAssert.AreNotEqual(encryptor1, encryptor2);
+        }
+
+        private ArgumentEncryptor PopulateEncryptor(string password)
+        {
+            ArgumentEncryptor encryptor = new ArgumentEncryptor(password);
+            encryptor.Add("Abc", "123");
+            encryptor.Add("Def", "456");
+            encryptor.Add("Ghi", "789");
+            encryptor.Add("Jkl", "0");
+            encryptor.Add("Abcdefghijklmnopqrstuvwxyz", "1234567890");
+            encryptor.Add("AbcdefghijklmnopqrstuvwxyzAbcdefghijklmnopqrstuvwxyzAbcdefghijklmnopqrstuvwxyz", "123456789012345678901234567890");
+            return encryptor;
+        }
+    }
+}
